ec4f12869682a7a6db3a6192ed99ab8500754bd8
Kalpika666 <Kalpikachampathi@gmail.com>
2025-05-06 23:55:56 +0530

    Delete verify.php

--- verify.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'config.php';
-$token = $_GET['token'];
-$stmt = $conn->prepare("UPDATE players SET is_verified = 1 WHERE verify_token = ?");
-$stmt->bind_param("s", $token);
-$stmt->execute();
-echo "Account verified!";
-?>
